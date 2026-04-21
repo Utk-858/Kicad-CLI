156e42da1d8cdbbacedb9110b73b18dae68de7bb
Utk-858 <utkarsh08bansal@gmail.com>
2026-04-21 17:32:55 +0530

    rag repository changed

--- fluxdiff/rag/config.py
+++ fluxdiff/rag/config.py
@@ -8,7 +8,7 @@ RAG_CONFIG = {
     # =========================
     # REPO SETTINGS
     # =========================
-    "repo_path": "/Users/utkarshbansal/KiParse",  # change later
+    "repo_path": "/Users/utkarshbansal/Chessboard-Hardware",  # change later
 
     # =========================
     # EMBEDDING SETTINGS
